4e53ad13e6bb7393683d89c5de0703aa67c24c60
Prashant <prashant.22910@gmail.com>
2018-09-16 15:26:32 +0530

    First commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,57 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+  
+  $scope.totalValue = 0;
+  $scope.storeValue = []
+  $scope.finalArray = []
+  $scope.numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  $scope.operationArray = [{value: 1, sign: '+'}, {value: 2, sign: '-'}, {value: 3, sign: '*'}, {value: 4, sign: '/'}]
+  
+  $scope.numberClick = function(value) {
 
+  	var count  = 0
+  	var number = 0
+  	$scope.storeValue.push(value)
+  	for (var i = $scope.storeValue.length-1; i >= 0; i--) {
+  		number = number + $scope.storeValue[i]*Math.pow(10, count)
+  		count++;
+  	}
+  	
+  	$scope.number = number;
+  }
+
+  $scope.operationClick = function(key) {
+
+  		$scope.storeValue = []
+	  	$scope.operator = key.value
+	  	$scope.finalArray.push($scope.number)
+  	}
+
+  	$scope.equalClick = function() {
+
+	  	var total = 0;
+	  	$scope.finalArray.push($scope.number)
+
+	  	console.log($scope.finalArray)
+
+	  	if ($scope.operator == 1) total = $scope.finalArray[0] + $scope.finalArray[1]
+	    else if ($scope.operator == 2) total = $scope.finalArray[0] - $scope.finalArray[1]
+		else if ($scope.operator == 3) total = $scope.finalArray[0] * $scope.finalArray[1]
+		else if ($scope.operator == 4) total = $scope.finalArray[0] / $scope.finalArray[1]
+
+		$scope.finalArray = []
+		$scope.number = total
+		$scope.totalValue = total
+
+		//console.log($scope.totalValue)
+	}
+
+	
+  
 }]);
+
+
+
+
